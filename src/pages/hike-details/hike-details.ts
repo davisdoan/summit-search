7b7e876a36cc88b0e7b0777f16f5654526fb16d7
jbrownssf <jpbrown@softstackfactory.org>
2017-05-16 05:08:30 +0000

    fixed hike details

--- src/pages/hike-details/hike-details.ts
+++ src/pages/hike-details/hike-details.ts
@@ -22,6 +22,7 @@ export class HikeDetails {
   hikeWeather;
   hikeImageUrl;
   hikeId;
+  hikeArray;
   showSave: boolean = false;
   
   constructor(
@@ -30,23 +31,27 @@ export class HikeDetails {
     public dataService: Data,
     public hikeResults: HikeResults
     ) {
-    this.hikeName = this.navParams.get('item').hikeName;
-    this.hikeElevation = this.navParams.get('item').hikeElevation;
-    this.hikeDistance = this.navParams.get('item').hikeDistance;
-    this.hikeWeather = this.navParams.get('item').hikeWeather;
-    this.hikeImageUrl = this.navParams.get('item').hikeImageUrl;
-    this.hikeId = this.navParams.get('item').id;
+    this.hikeArray = this.navParams.get('item');
+    this.hikeName = this.hikeArray[0].hikeName;
+    //this.hikeName = this.navParams.get('item').hikeName;
+    this.hikeElevation = this.hikeArray[0].hikeElevation;
+    this.hikeDistance = this.hikeArray[0].hikeDistance;
+    this.hikeWeather = this.hikeArray[0].hikeWeather;
+    this.hikeImageUrl = this.hikeArray[0].hikeImageUrl;
+    this.hikeId = this.hikeArray[0].id;
     //let comparisonHikes = window.localStorage.getItem("favHikes");
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HikeDetails');
-    this.hikeName = this.navParams.get('item').hikeName;
-    this.hikeElevation = this.navParams.get('item').hikeElevation;
-    this.hikeDistance = this.navParams.get('item').hikeDistance;
-    this.hikeWeather = this.navParams.get('item').hikeWeather;
-    this.hikeImageUrl = this.navParams.get('item').hikeImageUrl;
-    this.hikeId = this.navParams.get('item').id;
+    this.hikeArray = this.navParams.get('item');
+    this.hikeName = this.hikeArray[0].hikeName;
+    //this.hikeName = this.navParams.get('item').hikeName;
+    // this.hikeElevation = this.navParams.get('item').hikeElevation;
+    // this.hikeDistance = this.navParams.get('item').hikeDistance;
+    // this.hikeWeather = this.navParams.get('item').hikeWeather;
+    // this.hikeImageUrl = this.navParams.get('item').hikeImageUrl;
+    // this.hikeId = this.navParams.get('item').id;
     this.showSave = this.navParams.get("showSave");
   }
   
